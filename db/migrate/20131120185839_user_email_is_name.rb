8884b3df1ec6f02f7a141fae8fac2bd4c1a94246
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-11-20 12:44:43 -0800

    Update user_email_is_name migration.

--- db/migrate/20131120185839_user_email_is_name.rb
+++ db/migrate/20131120185839_user_email_is_name.rb
@@ -2,6 +2,6 @@ class UserEmailIsName < ActiveRecord::Migration
   def change
     User.delete_all
     Authentication.delete_all
-    add_column :users, :name
+    add_column :users, :name, :string, :default => nil
   end
 end
